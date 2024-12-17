94a4858f4ad2feb3e8f95032d0c7c2b8b0e59b54
zanwaar <zanwaarsamjid@gmail.com>
2024-12-17 12:41:28 +0700

    push

--- database/seeders/MerchantSeeder.php
+++ database/seeders/MerchantSeeder.php
@@ -14,6 +14,10 @@ class MerchantSeeder extends Seeder
     public function run(): void
     {
         $faker = \Faker\Factory::create();
+        $title = $faker->words(3, true); // Generate a three-word phrase
+        $slug = strtolower(str_replace(' ', '-', $title));
+
+
         DB::beginTransaction();
         try {
 
@@ -43,13 +47,100 @@ class MerchantSeeder extends Seeder
                 'owner_no_ktp' => $faker->unique()->numerify('################'),
                 'owner_ktp' => 'ktp2.jpg',
                 'merchant_nama' => 'VCO Desa Yainuelo',
-                'merchant_jenis' => 'Wholesale',
+                'merchant_jenis' => 'Kuliner',
                 'slug' => 'VCO-Desa-Yainuelo',
                 'merchant_alamat' => $faker->address,
                 'merchant_omzet' => $faker->numberBetween(100000, 10000000),
                 'merchant_foto' => 'vco.jpg',
                 'merchant_usaha' => 'usaha2.jpg',
                 'merchant_banner' => 'banner-vco.jpg',
+            ]);
+
+
+            Merchant::create([
+                'user_id' => 3,
+                'owner_nama_lengkap' => $faker->name,
+                'owner_no_hp' => $faker->phoneNumber,
+                'owner_alamat_lengkap' => $faker->address,
+                'owner_no_ktp' => $faker->unique()->numerify('################'),
+                'owner_ktp' => 'ktp2.jpg',
+                'merchant_nama' => 'Gagartang',
+                'merchant_jenis' => 'Kuliner',
+                'slug' => 'gagartang',
+                'merchant_alamat' => $faker->address,
+                'merchant_omzet' => $faker->numberBetween(100000, 10000000),
+                'merchant_foto' => 'gargantang.jpg',
+                'merchant_usaha' => 'gargantang.jpg',
+                'merchant_banner' => 'banner-gargantang.jpg',
+            ]);
+
+
+            Merchant::create([
+                'user_id' => 4,
+                'owner_nama_lengkap' => $faker->name,
+                'owner_no_hp' => $faker->phoneNumber,
+                'owner_alamat_lengkap' => $faker->address,
+                'owner_no_ktp' => $faker->unique()->numerify('################'),
+                'owner_ktp' => 'ktp2.jpg',
+                'merchant_nama' => 'Coklat sobeak',
+                'merchant_jenis' => 'Kuliner',
+                'slug' => $slug,
+                'merchant_alamat' => $faker->address,
+                'merchant_omzet' => $faker->numberBetween(100000, 10000000),
+                'merchant_foto' => 'gargantang.jpg',
+                'merchant_usaha' => 'gargantang.jpg',
+                'merchant_banner' => 'banner-gargantang.jpg',
+            ]);
+
+            Merchant::create([
+                'user_id' => 5,
+                'owner_nama_lengkap' => $faker->name,
+                'owner_no_hp' => $faker->phoneNumber,
+                'owner_alamat_lengkap' => $faker->address,
+                'owner_no_ktp' => $faker->unique()->numerify('################'),
+                'owner_ktp' => 'ktp2.jpg',
+                'merchant_nama' => 'Goda’an',
+                'merchant_jenis' => 'Kuliner',
+                'slug' =>    $slug,
+                'merchant_alamat' => $faker->address,
+                'merchant_omzet' => $faker->numberBetween(100000, 10000000),
+                'merchant_foto' => 'gargantang.jpg',
+                'merchant_usaha' => 'gargantang.jpg',
+                'merchant_banner' => 'banner-gargantang.jpg',
+            ]);
+
+            Merchant::create([
+                'user_id' => 6,
+                'owner_nama_lengkap' => $faker->name,
+                'owner_no_hp' => $faker->phoneNumber,
+                'owner_alamat_lengkap' => $faker->address,
+                'owner_no_ktp' => $faker->unique()->numerify('################'),
+                'owner_ktp' => 'ktp2.jpg',
+                'merchant_nama' => 'Geprek bung azka',
+                'merchant_jenis' => 'Kuliner',
+                'slug' =>    $slug,
+                'merchant_alamat' => $faker->address,
+                'merchant_omzet' => $faker->numberBetween(100000, 10000000),
+                'merchant_foto' => 'gargantang.jpg',
+                'merchant_usaha' => 'gargantang.jpg',
+                'merchant_banner' => 'banner-gargantang.jpg',
+            ]);
+
+            Merchant::create([
+                'user_id' => 7,
+                'owner_nama_lengkap' => $faker->name,
+                'owner_no_hp' => $faker->phoneNumber,
+                'owner_alamat_lengkap' => $faker->address,
+                'owner_no_ktp' => $faker->unique()->numerify('################'),
+                'owner_ktp' => 'ktp2.jpg',
+                'merchant_nama' => 'SeeCoffe.id',
+                'merchant_jenis' => 'Kuliner',
+                'slug' =>    $slug,
+                'merchant_alamat' => $faker->address,
+                'merchant_omzet' => $faker->numberBetween(100000, 10000000),
+                'merchant_foto' => 'gargantang.jpg',
+                'merchant_usaha' => 'gargantang.jpg',
+                'merchant_banner' => 'banner-gargantang.jpg',
             ]);
 
 
--- database/seeders/PermissionsDemoSeeder.php
+++ database/seeders/PermissionsDemoSeeder.php
@@ -38,12 +38,8 @@ class PermissionsDemoSeeder extends Seeder
         $role3 = Role::create(['name' => 'Super-Admin']);
         // gets all permissions via Gate::before rule; see AuthServiceProvider
 
-        // create demo users
-        $user = \App\Models\User::factory()->create([
-            'name' => 'januar samjid',
-            'email' => 'januarsamjid@gmail.com',
-        ]);
-        $user->assignRole($role);
+
+
         $user = \App\Models\User::factory()->create([
             'name' => 'Merchant 1',
             'email' => 'merchant1@example.com',
@@ -54,23 +50,96 @@ class PermissionsDemoSeeder extends Seeder
             'email' => 'merchant2@example.com',
         ]);
         $user->assignRole($role1);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Merchant 3',
+            'email' => 'merchant3@example.com',
+        ]);
+        $user->assignRole($role1);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Merchant 4',
+            'email' => 'merchant4@example.com',
+        ]);
+        $user->assignRole($role1);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Merchant 5',
+            'email' => 'merchant5@example.com',
+        ]);
+        $user->assignRole($role1);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Merchant 6',
+            'email' => 'merchant6@example.com',
+        ]);
+        $user->assignRole($role1);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Merchant 7',
+            'email' => 'merchant7@example.com',
+        ]);
+        $user->assignRole($role1);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Merchant 8',
+            'email' => 'merchant8@example.com',
+        ]);
+        $user->assignRole($role1);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Merchant 9',
+            'email' => 'merchant9@example.com',
+        ]);
+        $user->assignRole($role1);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Merchant 10',
+            'email' => 'merchant10@example.com',
+        ]);
+        $user->assignRole($role1);
 
+
+
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'januar samjid',
+            'email' => 'januarsamjid@gmail.com',
+        ]);
+        $user->assignRole($role);
         $user = \App\Models\User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
         ]);
         $user->assignRole($role2);
 
-        $user = \App\Models\User::factory()->create([
-            'name' => 'Super-Admin User',
-            'email' => 'superadmin@example.com',
-        ]);
-        $user->assignRole($role3);
+        // $user = \App\Models\User::factory()->create([
+        //     'name' => 'Super-Admin User',
+        //     'email' => 'superadmin@example.com',
+        // ]);
+        // $user->assignRole($role3);
 
-        for ($i = 0; $i < 100; $i++) {
-            $user = \App\Models\User::factory()->create();
-            $user->assignRole($role);
-        }
+
+        // $faker = \Faker\Factory::create();
+        // for ($i = 0; $i < 5; $i++) {
+        //     // Create a user
+        //     $user = \App\Models\User::factory()->create(); // Create user dynamically
+        //     $user->assignRole($role1); // Assign role to the user
+
+        //     // Generate merchant slug
+        //     $title = $faker->words(3, true);
+        //     $slug = strtolower(str_replace(' ', '-', $title));
+
+        //     // Create merchant associated with the user
+        //     \App\Models\Merchant::create([
+        //         'user_id' => $user->id, // Use the newly created user's ID
+        //         'owner_nama_lengkap' => $faker->name,
+        //         'owner_no_hp' => $faker->phoneNumber,
+        //         'owner_alamat_lengkap' => $faker->address,
+        //         'owner_no_ktp' => $faker->unique()->numerify('################'),
+        //         'owner_ktp' => 'ktp2.jpg',
+        //         'merchant_nama' => $faker->company, // Use company name for merchant name
+        //         'merchant_jenis' => 'Kuliner',
+        //         'slug' => $slug,
+        //         'merchant_alamat' => $faker->address,
+        //         'merchant_omzet' => $faker->numberBetween(100000, 10000000),
+        //         'merchant_foto' => 'default.jpg',
+        //         'merchant_usaha' => 'default.jpg',
+        //         'merchant_banner' => 'banner-default.jpg',
+        //     ]);
+        // }
         // \App\Models\User::factory(100)->create()->assignRole($role);
     }
 }
